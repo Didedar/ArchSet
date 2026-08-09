b6814f5c2e7bd83bdb2e15d10b353916f17ab451
Дархан Сабырхан <sabyrhandarhan@MacBook-Air-Gulnazira.local>
2026-08-09 19:51:48 +0500

    fix(test): remove unused mocktail import in welcome_page_test.dart

--- test/presentation/auth/pages/welcome_page_test.dart
+++ test/presentation/auth/pages/welcome_page_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 
 import 'package:archset_r2/core/localization/app_strings.dart';
 import 'package:archset_r2/presentation/auth/bloc/auth_bloc.dart';
